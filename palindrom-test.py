6852795b073ee4b0d2a94ac25629e57852c078e8
prithvisekhar <prithvisekhar@gmail.com>
2020-03-05 15:03:07 +0530

    To show coverage change

--- palindrom-test.py
+++ palindrom-test.py
@@ -5,9 +5,9 @@ from palindromCheck import palindromCheck
 class MyTest(unittest.TestCase):
     def test_my_function(self):
         self.assertEqual(palindromCheck('abc'), 0)
-        self.assertEqual(palindromCheck('aba'), 1)
-        self.assertEqual(palindromCheck('123'), 0)
-        self.assertEqual(palindromCheck('121'), 1)
+       # self.assertEqual(palindromCheck('aba'), 1)
+       # self.assertEqual(palindromCheck('123'), 0)
+       # self.assertEqual(palindromCheck('121'), 1)
         
 if __name__ == '__main__':
     unittest.main()
